61d31b7be751bbe09c9fd869844c155c59ba7b01
karthickr698 <rkarthick410@gmail.com>
2020-09-26 22:21:39 +0530

    added actor updation and get in controller and routes

--- app/controller/actorController.js
+++ app/controller/actorController.js
@@ -43,3 +43,66 @@ exports.addActor = (req, res) => {
       res.status(500).json({ error: true, errmsg: "Internal server Error" });
     });
 };
+
+exports.updateActor = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+      errormsg: "Please send required Details",
+      "Required fields": ["name", "gender", "DOB", "Bio"],
+      "sample Format": {
+        name: "test",
+        gender: "MALE",
+        DOB: "12/12/2020",
+        Bio: "testBio",
+      },
+    });
+  }
+  const name = req.body.name;
+  const gender = req.body.gender;
+  const DOB = req.body.DOB;
+  const Bio = req.body.Bio;
+
+  const actor_id = req.params.actor_id;
+
+  actorModel
+    .updateActorById(actor_id, name, gender, DOB, Bio)
+    .then(async (result) => {
+      if (result[0] === 0) {
+        res.status(400).json({ error: true, errmsg: "Invalid actorID given" });
+      } else {
+        const actorDetails = await actorModel.getActorById(actor_id);
+        res.send({
+          error: false,
+          result: actorDetails[0],
+          message: "actor updated successfully",
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: true, errmsg: "Internal server Error" });
+    });
+};
+
+exports.getActorDetails = (req, res) => {
+  const actor_id = req.params.actor_id;
+
+  actorModel
+    .getActorById(actor_id)
+    .then((result) => {
+      if (result.length === 0) {
+        res.status(400).json({ error: true, errmsg: "Invalid actorID given" });
+      } else {
+        res.send({
+          error: false,
+          result: result[0],
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: true, errmsg: "Internal server Error" });
+    });
+};
--- app/routes/actorRoutes.js
+++ app/routes/actorRoutes.js
@@ -3,10 +3,7 @@ const { body } = require("express-validator");
 
 const actorController = require("../controller/actorController");
 
-router.get(
-  "/",
-  actorController.getAllActors
-);
+router.get("/", actorController.getAllActors);
 
 router.post(
   "/add",
@@ -18,5 +15,18 @@ router.post(
   ],
   actorController.addActor
 );
+
+router
+  .route("/:actor_id")
+  .get(actorController.getActorDetails)
+  .put(
+    [
+      body("name").exists().bail().trim(),
+      body("gender").exists().bail().trim(),
+      body("DOB").exists().bail().trim(),
+      body("Bio").exists().bail().trim(),
+    ],
+    actorController.updateActor
+  )
 
 module.exports = router;
